347ab840a2a459204f889176990d62e12e4f613f
Fernando Montano <fernando.montano41@gmail.com>
2022-06-17 16:42:58 -0600

    Finished! WIP: Responsiveness.

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -5,13 +5,13 @@ export const Weather = ({ props = "" }) => {
   Object.keys(props).map((key) => (values = props[key]));
 
   return (
-    <div className="h-5/6 text-white">
+    <div className="h-5/6 text-white font-black drop-shadow-xl ">
       {Object.keys(values).length !== 0 && (
-        <div className="flex items-center flex-col text-center justify-evenly h-2/4">
+        <div className="flex items-center flex-col text-center justify-evenly h-2/4 p-4 rounded-xl">
           <div id="conditions" className="text-6xl tracking-widest">
             {values.values[0].conditions.toUpperCase()}
           </div>
-          <div id="location" className="text-4xl font-black drop-shadow-lg">
+          <div id="location" className="text-4xl ">
             {values.address}
           </div>
           <div id="temperature" className="text-5xl">
--- src/components/WeatherSearch/WeatherSearch.jsx
+++ src/components/WeatherSearch/WeatherSearch.jsx
@@ -25,21 +25,31 @@ export const WeatherSearch = () => {
   useEffect(() => {
     async function getPics() {
       const picResponse = await Unsplash.get("/search/photos", {
-        params: { query: searchPics, color: "black", orientation: "landscape", order_by: "relevant" },
+        params: { query: searchPics, color: "black", orientation: "landscape" },
       });
       setPics(picResponse.data.results);
     }
     if (searchPics) getPics();
   }, [searchPics]);
 
+  let randomValue = Math.floor(Math.random() * 10)
   return (
     // ⚠️ sending a function as props to the SearchBar.
     <div
       className="h-screen bg-cover bg-center transition-colors bg-black"
-      style={{ backgroundImage: pics.length === 0 ? "none" :`url(https://source.unsplash.com/${pics[Math.floor(Math.random() * 10)].id}` }}
+      style={{ backgroundImage: pics.length === 0 ? "none" :`url(https://source.unsplash.com/${pics[randomValue].id}` }}
     >
       <SearchBar onSubmit={onSearchBarSubmit} />
       <Weather props={result} />
+      <div>
+        {pics.length !== 0 && (
+          <div className="text-white flex justify-end  text-lg">
+            <div className="bg-slate-500 rounded-xl px-4 mr-4 py-2 bg-opacity-40">
+              Pic by: {pics[randomValue].user.first_name + " " + pics[randomValue].user.last_name}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
